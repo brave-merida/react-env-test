e042e6c76cbd0f221db92751b34a2530bb334b6e
besmir22 <besmir@dataskeptic.com>
2020-05-10 14:13:57 +0800

    added env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const logger = require('heroku-logger')
 
 function App() {
+  logger.info('NODE_ENV=', process.env.NODE_ENV);
+  logger.info('REACT_APP_TEST', process.env.REACT_APP_TEST);
   return (
     <div className="App">
       <header className="App-header">
